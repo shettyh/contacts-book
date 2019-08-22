cbd33580ee66b01483dee3df47634fcdec1c710d
Manjunath Shetty H <manjunathshetty@live.com>
2019-08-22 15:30:24 +0530

    Added configuration management

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.10
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/kr/pretty v0.1.0 // indirect
 )
---
+++ pkg/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"sync"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+const (
+	envPrefix = "CB"
+)
+
+var configInstance struct {
+	once     sync.Once
+	instance *Config
+}
+
+type Config struct {
+	DbHost     string `required: "true"` // CB_DBHOST
+	DbPort     string `default:"3306"`
+	DbUser     string `default:"root"`
+	DbPassword string
+	DbName     string `default:"contactsbook"`
+}
+
+func GetInstance() *Config {
+	configInstance.once.Do(func() {
+		configInstance.instance = new(Config)
+		err := envconfig.Process(envPrefix, &configInstance.instance)
+		if err != nil {
+			panic(err)
+		}
+	})
+	return configInstance.instance
+}
